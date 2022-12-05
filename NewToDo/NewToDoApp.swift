8247e93bbf8dd610dd0644aa0dec51e0d59e00ce
Elijah Patric <foreverelijah@gmail.com>
2022-12-05 14:57:47 -0600

    Initial Commit

---
+++ NewToDo/NewToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewToDoApp.swift
+//  NewToDo
+//
+//  Created by Elijah on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct NewToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
